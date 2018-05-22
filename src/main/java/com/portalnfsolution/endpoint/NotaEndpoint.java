abc4a833aaad32ad1d5eb4d518b2b9a2c2d02231
felipe.simmi <simmifelipe1992@gmail.com>
2018-05-22 11:01:43 -0300

    Criados varios metodos de pesquisa de notas na interface de NotaRepository e consequentemente metodos de acesso no NotaEndpoint para expor estes dados.

--- src/main/java/com/portalnfsolution/endpoint/NotaEndpoint.java
+++ src/main/java/com/portalnfsolution/endpoint/NotaEndpoint.java
@@ -1,12 +1,15 @@
 package com.portalnfsolution.endpoint;
 
+import com.portalnfsolution.model.Nota;
 import com.portalnfsolution.repository.NotaRepository;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.time.LocalDate;
 
 /**
  * Created by felipe.simmi on 21/05/2018.
@@ -27,6 +30,67 @@ public class NotaEndpoint {
     public ResponseEntity<?> listarTodos(){
         return new ResponseEntity<>(repository.findAll(), HttpStatus.OK);
     }
+
+    @GetMapping("/protegido/notas/{cnpj}")
+    public ResponseEntity<?> porCnpj(@PathVariable("cnpj") String cnpj){
+        return new ResponseEntity<>(repository.findNotaByCnpjEmitente(cnpj), HttpStatus.OK);
+    }
+
+    @GetMapping("/protegido/notas/{cnpj}/{dataEmissao}")
+    public ResponseEntity<?> porCnpjEDataEmissao(
+            @PathVariable("cnpj") String cnpj, @PathVariable("dataEmissao") String dataEmissao){
+        LocalDate data = LocalDate.parse(dataEmissao);
+        return new ResponseEntity<>(repository.findNotaByCnpjEmitenteAndDataEmissao(cnpj, data), HttpStatus.OK);
+    }
+
+    @GetMapping("/protegido/notas/data/{dataEmissao}")
+    public ResponseEntity<?> porDataEmissao(@PathVariable("dataEmissao") String dataEmissao){
+        LocalDate data = LocalDate.parse(dataEmissao);
+        return new ResponseEntity<>(repository.findNotaByDataEmissao(data), HttpStatus.OK);
+    }
+
+    @GetMapping("/protegido/notas/situacao/{situacao}")
+    public ResponseEntity<?> porSituacao(@PathVariable("situacao") String situacao){
+        return new ResponseEntity<>(repository.findNotaBySituacao(situacao), HttpStatus.OK);
+    }
+
+    @GetMapping("/protegido/notas/chave/{chave}")
+    public ResponseEntity<?> porChave(@PathVariable("chave") String chave){
+        return new ResponseEntity<>(repository.findNotaByChave(chave), HttpStatus.OK);
+    }
+
+    @GetMapping("/protegido/notas/destinatario/{destinatario}")
+    public ResponseEntity<?> porDestinatario(@PathVariable("destinatario") String destinatario){
+        return new ResponseEntity<>(repository.findNotaByDestinatarioIgnoringCaseContaining(destinatario), HttpStatus.OK);
+    }
+
+    @GetMapping("/protegido/notas/{cnpj}/{numero}/{serie}")
+    public ResponseEntity<?> porCnpjENumeroESerie(
+            @PathVariable("cnpj") String cnpj, @PathVariable("numero") String numero, @PathVariable("serie") String serie){
+        Integer sr = Integer.valueOf(serie);
+        numero = StringUtils.leftPad(numero, 9, "0");
+        return new ResponseEntity<>(repository.findNotaByCnpjEmitenteAndNumeroAndSerie(cnpj, numero, sr), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/protegido/notas", method = RequestMethod.POST)
+    public ResponseEntity<?> salvar(@RequestBody Nota nota) {
+        return new ResponseEntity<>(repository.save(nota), HttpStatus.CREATED);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 }
--- src/main/java/com/portalnfsolution/repository/NotaRepository.java
+++ src/main/java/com/portalnfsolution/repository/NotaRepository.java
@@ -3,10 +3,26 @@ package com.portalnfsolution.repository;
 import com.portalnfsolution.model.Nota;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface NotaRepository extends PagingAndSortingRepository<Nota, Long> {
 
     List<Nota> findAll();
+
+    List<Nota> findNotaByCnpjEmitente(String cnpj);
+
+    List<Nota> findNotaByCnpjEmitenteAndDataEmissao(String cnpj, LocalDate dataEmissao);
+
+    List<Nota> findNotaByDataEmissao(LocalDate dataEmissao);
+
+    List<Nota> findNotaBySituacao(String situacao);
+
+    List<Nota> findNotaByChave(String chave);
+
+    List<Nota> findNotaByDestinatarioIgnoringCaseContaining(String destinatario);
+
+    List<Nota> findNotaByCnpjEmitenteAndNumeroAndSerie(String cnpj, String numero,Integer serie);
+
 
 }
